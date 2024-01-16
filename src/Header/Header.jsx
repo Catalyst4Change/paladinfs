463c24f4567eb2f5b42f32d85cc4c8a9049d4728
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2024-01-16 12:06:56 -0700

    complete header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,34 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Header.scss"
+import quote from "../assets/quote.png"
+import phone from "../assets/phone.png"
+import directions from "../assets/directions.png"
 
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
   return (
     <main className="header">
-      <section className="header-item">
-        <div className="icon-container">
-          <img src="" alt="hamburger menu" />
+      <section className="header-menu">
+        <div className="hamburger" onClick={toggleMenu}>
+          <div
+            className={`hamburger-slice line-1 ${menuOpen ? "active" : ""}`}
+          ></div>
+          <div
+            className={`hamburger-slice line-2 ${menuOpen ? "active" : ""}`}
+          ></div>
+          <div
+            className={`hamburger-slice line-3 ${menuOpen ? "active" : ""}`}
+          ></div>
         </div>
-      </section>
-      <section className="header-item">
-        <h1>Paladin Fiduciary Services LLC</h1>
-      </section>
-      <section className="header-item">
-        <div className="icon-container">
-          <img src="" alt="list icon" />
-        </div>
-        <span>Get Quote</span>
-      </section>
-      <section className="header-item">
-        <div className="icon-container">
-          <img src="" alt="phone icon" />
-        </div>
-        <span>Call Now</span>
+        <h1 className="header-title">Paladin Fiduciary Services LLC</h1>
       </section>
       <section className="header-item">
         <div className="icon-container">
-          <img src="" alt="arrow sign icon" />
+          <img src={quote} alt="list icon" />
         </div>
-        <span>Get Directions</span>
+        <span className="header-item-text">Get Quote</span>
+      </section>
+      <section className="header-item">
+        <div className="icon-container">
+          <img src={phone} alt="phone icon" />
+        </div>
+        <span className="header-item-text">Call Now</span>
+      </section>
+      <section className="header-item">
+        <div className="icon-container">
+          <img src={directions} alt="arrow sign icon" />
+        </div>
+        <span className="header-item-text">Get Directions</span>
       </section>
     </main>
   )
